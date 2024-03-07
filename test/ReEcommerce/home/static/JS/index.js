91feaa0684e9313633fd4d057ac54d10f95f3c5e
Mirav.21 <109288213+Mirav21@users.noreply.github.com>
2024-03-07 16:30:12 +0530

    Create index.js

---
+++ test/ReEcommerce/home/static/JS/index.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+var counter=1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+
+    if(counter > 4)
+    {
+        counter=1;
+    }
+},4000);
