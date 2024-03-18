320d074956506e97b832de3ec5b19da5b9e4fb74
[DevUpadhyay1] <devupadhyay480@gmail.com>
2024-03-18 12:42:43 +0530

    push

--- test/ReEcommerce/home/firstapp/views.py
+++ test/ReEcommerce/home/firstapp/views.py
@@ -20,3 +20,18 @@ def Index(request):
         # Add other image paths as needed
     ]
     return render(request, 'Index.html', {'image_paths': image_paths})
+
+
+# def my_view(request):
+#     data = [
+#         {"image_src": "Furniturerental.png", "link": "rent.html"},
+#         {"image_src": "banglow.jpg", "link": "#"},
+#         {"image_src": "furniture.jpg", "link": "#"},
+#         {"image_src": "Furniturerental.png", "link": "#"},
+#         {"image_src": "CarRental.png", "link": "#"},
+#         {"image_src": "banglow.png", "link": "#"},
+#         {"image_src": "CarRental.webp", "link": "#"},
+#         {"image_src": "mainback.png", "link": "#"},
+#         {"image_src": "Furniturerental.png", "link": "#"},
+#     ]
+#     return render(request, 'Index.html', {'data': data})
