4b85ae458e2efc723fb84dcc5bd0e3fc301df7af
Mirav.21 <109288213+Mirav21@users.noreply.github.com>
2024-04-04 20:45:35 +0530

    2nd Presentation html

--- ReEcommerce/home/static/JS/index.js
+++ ReEcommerce/home/static/JS/index.js
@@ -2,20 +2,19 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');  
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
 }
 
-var counter=1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
+var counter = 1;
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
 
-    if(counter > 7)
-    {
-        counter=1;
-    }
-},4000);
+  if (counter > 7) {
+    counter = 1;
+  }
+}, 4000);
 
 
 
@@ -30,11 +29,11 @@ function previewImage() {
   const file = fileInput.files[0];
   if (file) {
     const reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       const img = document.createElement('img');
       img.src = e.target.result;
       img.alt = 'Preview';
-      img.style.maxWidth = '200px'; 
+      img.style.maxWidth = '200px';
       imagePreview.appendChild(img);
       changeButton.style.display = 'inline-block';
       uploadButton.style.display = 'none';
@@ -47,41 +46,53 @@ function previewImage() {
 
 // animations
 
-window.addEventListener('scroll',reveal);
+window.addEventListener('scroll', reveal);
 
-function reveal(){
+function reveal() {
   var reveals = document.querySelectorAll('.reveal');
 
-  for(var i=0; i<reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
     var windowheight = window.innerHeight;
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 100;
 
-    if(revealtop < windowheight-revealpoint){
+    if (revealtop < windowheight - revealpoint) {
       reveals[i].classList.add('active');
     }
-    else{
+    else {
       reveals[i].classList.remove('active');
     }
   }
 }
 
 
+// Ratting with 5 star
 
-  let rating = 0;
+let ratting = 0;
 
-  function rate(stars) {
-    rating = stars;
-    let allStars = document.querySelectorAll('.star');
-    for (let i = 0; i < allStars.length; i++) {
-      if (i < stars) {
-        allStars[i].classList.add('gold');
-      } else {
-        allStars[i].classList.remove('gold');
-      }
+function rate(stars) {
+  ratting = stars;
+  let allStars = document.querySelectorAll('.star');
+  for (let i = 0; i < allStars.length; i++) {
+    if (i < stars) {
+      allStars[i].classList.add('gold');
+    } else {
+      allStars[i].classList.remove('gold');
     }
   }
+}
 
-  document.querySelectorAll('.star')[3].addEventListener('click', function() {
-    rate(4);
-  });
+document.querySelectorAll('.star')[3].addEventListener('click', function () {
+  rate(4);
+});
+
+
+
+function togglePopup() {
+  var popup = document.getElementById("popup");
+  if (popup.style.display === "none" || popup.style.display === "") {
+    popup.style.display = "block";
+  } else {
+    popup.style.display = "none";
+  }
+}
